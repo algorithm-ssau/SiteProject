c316b2fe3c9195850050bd10ee6ce947407b5e6f
SemaginaPolina <61327588+SemaginaPolina@users.noreply.github.com>
2020-03-08 14:51:21 +0400

    Update WorkWithDB.py

--- WorkWithDB.py
+++ WorkWithDB.py
@@ -1,44 +1,63 @@
 import pymongo
-class WorkWithDB:
-    ''''Класс результата операции. Сюда заносить isGood = true, если всё хорошо
-    и обе переменные, если есть косяки и операция не получилась'''
-    class Result:
-        isGood
-        ErrorMessage
+from pymongo import MongoClient
+import traceback
+
+class Result():        
 
         def __init__(self, isGood, ErrorMessage):
-            self.isGood = isGood
+            self.isGood = bool(isGood)
             self.ErrorMessage = ErrorMessage
+        
+        def getErrorMessage(self): return self.ErrorMessage 
 
-        @staticmethod
-        def isGood(): return isGood
-
-        @staticmethod
-        def getErrorMessage(): return ErrorMessage
-
-        @staticmethod
-        def setIsGoodVariable(isGood):
+        def setIsGoodVariable(self, isGood):
             self.isGood = isGood
 
-        @staticmethod
-        def setErrorMessage(ErrorMessage):
+        def setErrorMessage(self, ErrorMessage):
             self.ErrorMessage = ErrorMessage
 
-
-    @staticmethod
-    def AddToDatabase(city, isTeacher, user):
-        res = Result(True, 'non')
+class WorkWithDB():
+    ''''Класс результата операции. Сюда заносить isGood = true, если всё хорошо
+    и обе переменные, если есть косяки и операция не получилась'''
+    
+    def AddToDatabase(self, city, isTeacher, user):
+        res = Result(True, ' ')
 
         try:
-            from pymongo import MongoClient
+            
             client = MongoClient()
             db = client['UsersDB']
 
             if isTeacher:
                 nameCollect = city+'teachers'
-                collection = db.[nameCollect]
-                db.collection.insert_one(usre)
-    
+                collect = db[nameCollect]
+                collect.insert_one(user)
 
-    
-    
+                
+
+            else:
+                nameCollect = city+'students'
+                collect = db[nameCollect]
+                collect.insert_one(user)
+
+            number = user.get("Telephone")
+            doc ={"Telephone": str(number)}
+            teachersNum = db.PhoneNumber
+            teachersNum.insert_one(doc)
+
+            login = user.get("Login")
+            doc_log ={"Login": str(login)}
+            usersLogin = db.Login
+            usersLogin.insert_one(doc_log)
+
+            res = Result(True, "")
+
+
+        except Exception :
+            res = Result(False, traceback.print_exc())
+
+        return res
+            
+test = WorkWithDB()
+user = {"name":"Vladimir", "Telephone":"89277530000", "Login":"NiceLogin"}
+test.AddToDatabase("SAMARA", False, user)
