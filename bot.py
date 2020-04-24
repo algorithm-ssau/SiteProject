ef85ae3f426158f85db06bd760b56015cbdae03b
SemaginaPolina <61327588+SemaginaPolina@users.noreply.github.com>
2020-04-24 12:42:57 +0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,40 @@
+from random import randint
+
+class messageBot():
+    @staticmethod
+    def Welcome(IDuser):
+        welcomeList = ['Привет, друг!','И снова здрвствуй!','Давно не виделись!','Доброго времени суток!','Привет. Как жизнь?','Приветствую!']
+        welcomeMessage = str(welcomeList[randint(0, len(welcomeList)-1)])+' Меня зовут Тимофей, для друзей Тимоха и я могу тебе помочь в некоторых вопросах.\nВ каком предмете тебе нужна помощь?\n1.Литература\n2.Русский язык\n3.Математика\n0.Хочу написать разработчикам'
+        print(welcomeMessage)
+
+    @staticmethod
+    def MainMenu(menu):
+        
+        menuList = {
+            1 : messageBot.literature,
+            2 : messageBot.rus,
+            3 : messageBot.math,
+            0 : messageBot.creator
+        }
+
+        func = menuList.get(menu)
+        func()
+
+    @staticmethod
+    def creator():
+        print('creator')
+
+    @staticmethod
+    def literature():
+        print('litrature')
+
+    @staticmethod
+    def rus():
+        print('rus')
+
+    @staticmethod
+    def math():
+        print('math')
+
+messageBot.Welcome(5)
+messageBot.MainMenu(3)
